ac73cec4fb0b2cd517c10a03a675699023b8cfe9
chadbarrows <chad.m.barrows@gmail.com>
2018-08-19 10:12:20 -0400

    mainly style changes - aug 18

--- js/app.js
+++ js/app.js
@@ -36,3 +36,9 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+function success() {
+    console.log('success');
+}
+
+document.getElementById('board').addEventListener('click', success);
